7fa8a7717bf51733fd64e3b8fa42635a96950b9a
bgauryy <bgauryy@gmail.com>
2025-08-28 00:10:31 +0300

    impr vercel local server test

--- packages/octocode-local-server/rollup.config.js
+++ packages/octocode-local-server/rollup.config.js
@@ -9,9 +9,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 export default {
   input: 'api/index.ts',
   output: {
-    file: 'api/index.js',
-    format: 'esm',
-    sourcemap: !isProduction
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: !isProduction, // Enable source maps for development
+    banner: '#!/usr/bin/env node'
   },
   plugins: [
     nodeResolve({
